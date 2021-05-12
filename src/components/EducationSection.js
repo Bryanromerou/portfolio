466981e54a9a2193f313261860f7cc1e72eb5284
Bryanromerou <bryanromerou@gmail.com>
2021-05-11 18:23:09 -0700

    adds Redcross to experience section

--- src/components/EducationSection.js
+++ src/components/EducationSection.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import dominguez_hills from '../icons/logos/dh.png';
-import elac from '../icons/logos/elacLogo.png';
 import general_assembly from '../icons/logos/gaLogo.png';
 
 
--- src/components/ExperienceSection.js
+++ src/components/ExperienceSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import the_fed from '../icons/logos/frbsfLogoblack.png';
 import wells_fargo from '../icons/logos/wellsLogo.png';
 import general_assembly from '../icons/logos/gaLogo.png';
+import red_cross from '../icons/logos/arclogo.png';
 
 const ExperienceSection = () => {
   return (
@@ -14,6 +15,16 @@ const ExperienceSection = () => {
           <p>• Support students in understanding computer programming fundamentals such as object-oriented programming, data structures, and coding best practices.</p>
           <p>• Aid in implementation of full stack technologies and languages and frameworks such as Python, Django, SQL, Express, React, Node, HTML, CSS, JavaScript, Third Party APIs, etc.</p>
           <p>• Guide students in troubleshooting/debugging their full stack applications and API’s.</p>
+        </div>
+      </div>
+      <div className = "experience-listing">
+        <img src={red_cross} alt="Red Cross" className="logo-v2"/>
+        <p className="listing-title"><span>RedCross</span> - Code4Good Volunteer [2021-Present]</p>
+        <div className="experience-description">
+          <p>• Partnered with <a href="https://www.intel.com/content/www/us/en/research/overview.html">Intel Labs</a> to build a full stack application that uses Natural Language Processing to analyze and visualize RedCross survey data to better use our volunteering resources.</p>
+          <p>• Merged a Pull Request for <i>Red Cross Survey Analytics Dashboard</i> for trial deployment at the Peruvian RedCross.</p>
+          <p>• Helped fix issues with React, Express, and MongoDB.</p>
+          <p>• Used Jira and Kanban methodologies to keep track of Project’s progress.</p>
         </div>
       </div>
       <div className = "experience-listing">
